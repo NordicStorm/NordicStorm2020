d271e6b63dfc9d1854d6e4548715e1bac9714b9a
NordicStorm <nordicstorm3018@saintpeterrobotics.org>
2021-03-09 20:35:56 -0600

    follow fix

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -65,7 +65,7 @@ shootTrigger.whileHeld(new CenterAndShoot());
 rightJoystick = new Joystick(0);
 
 trigger = new JoystickButton(rightJoystick, 1);
-trigger.whileHeld(new FollowBall(true));
+trigger.whileHeld(new FollowBall(true, false));
 
 
         // SmartDashboard Buttons
