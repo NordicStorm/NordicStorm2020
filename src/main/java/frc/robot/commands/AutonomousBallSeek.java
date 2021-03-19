57d091d69f0f5320043d5d01e3722600893a402f
NordicStorm <nordicstorm3018@saintpeterrobotics.org>
2021-03-18 19:44:58 -0500

    red A better

--- src/main/java/frc/robot/commands/AutonomousBallSeek.java
+++ src/main/java/frc/robot/commands/AutonomousBallSeek.java
@@ -118,15 +118,15 @@ public class AutonomousBallSeek extends AutoWithInit {
         group.addSequential(new FollowBall(false, true));
         //addSequential(new TurnToAngle(270, 5));
         group.addSequential(new MultiPartPath(26, false, false)
-            .addRawArc(270, -0.5, 0.5, false, false, true)
+            .addRawArc(-10, -0.5, 0.5, false)
             .finalizePath());
 
-        group.addSequential(new FollowBall(false, true));
+        group.addSequential(new FollowBall(false, true, 0.5));
         //group.addSequential(new TurnToAngle(0, 5));
         group.addSequential(new MultiPartPath(0, false, true)
-            .addRawArc(0, -0.5, 0.5, true, false, true)
+            .addRawArc(-45, -0.5, 0.5, true)
             .atMaxSpeed()
-            .addStraight(14, false)
+            .addStraight(14, false, 0)
             .atMaxSpeed()
             .finalizePath());
         group.start();
